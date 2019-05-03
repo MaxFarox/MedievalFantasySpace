e11644070e4440af3fe8ae9dc0c8c26edb5b71de
Tontaco <Tontaco@users.noreply.github.com>
2019-05-03 20:07:34 +0200

    Project Created

---
+++ Proto Auralux/Assets/Scripts/planetScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class planetScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _timeSpawn;
+
+    [SerializeField]
+    private float _energyPerSecond;
+
+    [SerializeField]
+    private float _level;
+
+    public int planetState = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnTroop();
+        }
+    }
+
+    public void SpawnTroop()
+    {
+        Debug.Log("SPAWN");
+        var troop = Resources.Load<GameObject>("Troop");
+        Instantiate(troop, this.transform.position, Quaternion.identity);
+       
+    }
+
+    public float GetEnergy()
+    {
+        return _energyPerSecond;
+    }
+}
---
+++ Proto Auralux/Assets/troopScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class troopScript : MonoBehaviour
+{
+
+    private NavMeshAgent _agent;
+    private Vector3 _destination;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = this.GetComponent<NavMeshAgent>();
+        _destination = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _agent.destination = _destination;
+    }
+
+    public void SetDestination(Vector3 dest)
+    {
+        _destination = dest;
+    }
+}
